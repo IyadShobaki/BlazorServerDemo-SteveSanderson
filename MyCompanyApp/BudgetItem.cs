a7f681bc411868b78b72eeef183cb8120f669d53
IyadShobaki <iyadshobaki82@gmail.com>
2020-07-27 15:10:07 -0400

    Initial commit

---
+++ MyCompanyApp/BudgetItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompanyApp
+{
+    public class BudgetItem
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
